d2c0ddcecb87a103c70f1b02d3dbfc55a8cbdbea
Jared Hanson <jaredhanson@gmail.com>
2014-03-13 14:30:20 -0700

    Rename blueprint to project.

--- test/package.test.js
+++ test/package.test.js
@@ -1,11 +1,21 @@
 /* global describe, it, expect */
 
-var tensile = require('..');
+var tensile = require('..')
+  , Project = require('../lib/project');
 
 describe('tensile', function() {
   
+  it('should expose singleton application', function() {
+    expect(tensile).to.be.an('object');
+    expect(tensile).to.be.an.instanceOf(Project);
+  });
+  
   it('should export version', function() {
     expect(tensile.version).to.be.a('string');
+  });
+  
+  it('should export constructors', function() {
+    expect(tensile.Project).to.be.a('function');
   });
   
 });
