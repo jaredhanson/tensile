e864b1ff0e43bc9954cd5c5bd8176a76ab7cbb30
Jared Hanson <jaredhanson@gmail.com>
2014-03-14 10:42:00 -0700

    In-progress refactoring to applier.

--- test/package.test.js
+++ test/package.test.js
@@ -1,14 +1,8 @@
 /* global describe, it, expect */
 
-var tensile = require('..')
-  , Project = require('../lib/project');
+var tensile = require('..');
 
 describe('tensile', function() {
-  
-  it('should expose singleton application', function() {
-    expect(tensile).to.be.an('object');
-    expect(tensile).to.be.an.instanceOf(Project);
-  });
   
   it('should export version', function() {
     expect(tensile.version).to.be.a('string');
